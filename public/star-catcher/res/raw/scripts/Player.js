b0794060be5052c376f73c3eac7cc3be17d2cd0c
Nan Wang <nantas@gmail.com>
2015-12-13 22:46:11 +0800

    add star catcher demo.

---
+++ public/star-catcher/res/raw/scripts/Player.js
@@ -0,0 +1,154 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        jumpHeight: 0,
+        jumpDuration: 0,
+        maxMoveSpeed: 0,
+        accel: 0,
+        scoreDisplay: {
+            default: null,
+            type: cc.ELabel
+        }
+    },
+
+    // use this for initialization
+    onLoad: function () {
+        // variables to store player status
+        this.xSpeed = 0;
+        // control input flags
+        this.accLeft = false;
+        this.accRight = false;
+        // screen boundaries
+        this.minPosX = -this.node.parent.width/2;
+        this.maxPosX = this.node.parent.width/2;
+        // player score
+        this.score = 0;
+
+        // set jump action
+        this.jumpAction = this.setJumpAction();
+
+        // input management
+        this.setInputControl();
+    },
+
+    setInputControl: function () {
+        var self = this;
+        //add keyboard input listener to jump, turnLeft and turnRight
+        cc.eventManager.addListener({
+            event: cc.EventListener.KEYBOARD,
+            // set a flag when key pressed
+            onKeyPressed: function(keyCode, event) {
+                switch(keyCode) {
+                    case cc.KEY.a:
+                    case cc.KEY.left:
+                        self.accLeft = true;
+                        self.accRight = false;
+                        break;
+                    case cc.KEY.d:
+                    case cc.KEY.right:
+                        self.accLeft = false;
+                        self.accRight = true;
+                        break;
+                }
+            },
+            // unset a flag when key released
+            onKeyReleased: function(keyCode, event) {
+                switch(keyCode) {
+                    case cc.KEY.a:
+                    case cc.KEY.left:
+                        self.accLeft = false;
+                        break;
+                    case cc.KEY.d:
+                    case cc.KEY.right:
+                        self.accRight = false;
+                        break;
+                }
+            }
+        }, self);
+
+        // touch input
+        cc.eventManager.addListener({
+            event: cc.EventListener.TOUCH_ONE_BY_ONE,
+            onTouchBegan: function(touch, event) {
+                var touchLoc = touch.getLocation();
+                if (touchLoc.x >= cc.winSize.width/2) {
+                    self.accLeft = false;
+                    self.accRight = true;
+                } else {
+                    self.accLeft = true;
+                    self.accRight = false;
+                }
+                // don't capture the event
+                return true;
+            },
+            onTouchEnded: function(touch, event) {
+                self.accLeft = false;
+                self.accRight = false;
+            }
+        }, self);
+    },
+
+    setJumpAction: function () {
+        // jump action
+        var jumpUp = cc.moveBy(this.jumpDuration, cc.p(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
+        var jumpDown = cc.moveBy(this.jumpDuration, cc.p(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
+        // var callback = cc.callFunc(this.onJumpEnd, this);
+        return cc.repeatForever(cc.sequence(jumpUp, jumpDown));
+    },
+
+    getCenterPos: function () {
+        var centerPos = cc.p(this.node._sgNode.x, this.node._sgNode.y + this.node.height/2);
+        return centerPos;
+    },
+
+    gainScore: function () {
+        this.score += 1;
+        this.scoreDisplay.string = 'Score: ' + this.score.toString();
+    },
+
+    resetScore: function () {
+        this.score = 0;
+        this.scoreDisplay.string = 'Score: ' + this.score.toString();
+    },
+
+    startMove: function (pos) {
+        this.enabled = true;
+        this.resetScore();
+        this.xSpeed = 0;
+        this.node.setPosition(pos);
+        this.node._sgNode.runAction(this.jumpAction);
+    },
+
+    stopMove: function () {
+        this.node._sgNode.stopAllActions();
+    },
+
+    // called every frame
+    update: function (dt) {
+        // get current speed with control flag
+        // player need to keep pressing to accelerate
+        if (this.accLeft) {
+            this.xSpeed -= this.accel * dt;
+        } else if (this.accRight) {
+            this.xSpeed += this.accel * dt;
+        }
+
+        if ( Math.abs(this.xSpeed) > this.maxMoveSpeed ) {
+            // if speed reach limit, use max speed with current direction
+            this.xSpeed = this.maxMoveSpeed * this.xSpeed / Math.abs(this.xSpeed);
+        }
+
+        // update player position
+        this.node.x += this.xSpeed * dt;
+
+        // limit player position inside screen
+        if ( this.node.x > this.maxPosX) {
+            this.node.x = this.maxPosX;
+            this.xSpeed = 0;
+        } else if (this.node.x < this.minPosX) {
+            this.node.x = this.minPosX;
+            this.xSpeed = 0;
+        }
+    },
+});
